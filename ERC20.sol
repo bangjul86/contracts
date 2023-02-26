85c6078e9e519ed2a3237ac292839e02c84ec0e5
Bang Jul <126306580+bangjul86@users.noreply.github.com>
2023-02-26 16:19:54 +0700

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -2,7 +2,7 @@
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract StakingBooster is ERC20, Ownable {
+contract StakingBooster is ERC20 {
     constructor(string memory _name, string memory _symbol)
         ERC20(_name, _symbol)
     {
